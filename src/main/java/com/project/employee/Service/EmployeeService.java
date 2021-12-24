b5ef75176df1f8e9b5f265fba3907dcdc67d013b
chanda <chanda.labh65@gmail.com>
2021-12-24 12:53:07 +0530

    "third commit"

--- src/main/java/com/project/employee/Service/EmployeeService.java
+++ src/main/java/com/project/employee/Service/EmployeeService.java
@@ -1,13 +1,12 @@
 package com.project.employee.Service;
+
+import java.util.List;
+
 import com.project.employee.model.EmployeeModel;
 
 public interface EmployeeService {
-	
+
 	String addEmployee(EmployeeModel employeeModel);
+	List<EmployeeModel> findAllEmployee();
+	
 }
-
-
-
-
-	
-	
